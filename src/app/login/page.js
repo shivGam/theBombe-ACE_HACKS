1bb64a89841871957967106bfcbb0faf369e2c50
Harsh Chaudhari <harshchaudhari150@gmail.com>
2024-03-18 22:54:27 +0530

    all files

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,67 +1,87 @@
-import React from "react";
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
 
 export const Login = () => {
+  const cookie = new Cookies();
+  const router = useRouter();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(
+      "http://localhost:4000/api/users/login",
+      data
+    );
+    if (response.data) {
+      alert("Login Successful");
+      cookie.set("user", response.data, {
+        maxAge: 60 * 60 * 24,
+      });
+      router.push("/");
+    }
+  };
+
   return (
     <div className="mx-4 my-8">
-      <div class="flex h-full flex-wrap items-center justify-center lg:justify-between">
-        <div class="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+      <div className="flex h-full flex-wrap items-center justify-center lg:justify-between">
+        <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
           <img
-            src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-            class="w-full"
+            src="/logo.jpg"
+            className="w-full bg-transparent rounded-full"
             alt="Sample image"
           />
         </div>
+        <h3 className="block dark:text-white text-[#349404] text-2xl font-bold mb-4">
+          Farm Buddy | the Bombe
+        </h3>
       </div>
-      <form class="max-w-sm  m-10 mx-auto">
-        <div class="mb-5">
+      <form
+        className="max-w-sm  m-10 mx-auto"
+        onSubmit={handleSubmit} // Use handleSubmit function for form submission
+      >
+        <div className="mb-5">
           <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="email"
+            className="block dark:text-white text-[#349404] text-lg font-bold mb-4"
           >
             Your email
           </label>
           <input
             type="email"
             id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
+            onChange={(e) => setData((p) => ({ ...p, email: e.target.value }))}
           />
         </div>
-        <div class="mb-5">
+        <div className="mb-5">
           <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="password"
+            className="block dark:text-white text-[#349404] text-lg font-bold mb-4"
           >
             Your password
           </label>
           <input
             type="password"
             id="password"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
+            onChange={(e) =>
+              setData((p) => ({ ...p, password: e.target.value }))
+            }
           />
         </div>
-        <div class="flex flex-col items-start mb-5">
-          <div class="flex items-center">
-            <input
-              id="remember"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-              required
-            />
-            <label
-              for="remember"
-              class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Remember me
-            </label>
-          </div>
-        </div>
+
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         >
           Submit
         </button>
--- src/components/Help.js
+++ src/components/Help.js
@@ -6,21 +6,21 @@ const Help = () => {
     <div>
       <h3 className="text-[#349404] text-lg font-bold">Get Help</h3>
       <div className="w-full flex items-center gap-8 overflow-x-auto my-4">
-        {Array(10)
-          .fill()
-          .map((_, index) => {
+        {["Sarpanch", "Deputy Sarpanch", "Gram Sevak", "G.P. Members"].map(
+          (item, index) => {
             return (
-              <div className="relative bg-transparent w-full">
+              <div className="relative bg-transparent w-full" key={index}>
                 <Image
                   width={50}
                   height={50}
                   src={"/user.png"}
                   className="rounded-full "
                 />
-                <p className="font-semibold text-black mt-2">Gram</p>
+                <p className="font-semibold text-black mt-2">{item}</p>
               </div>
             );
-          })}
+          }
+        )}
       </div>
     </div>
   );
